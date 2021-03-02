2ba2733e78646d014e46fd7ed3a75fe27bb2330d
lcsol <chao.lin0076@gmail.com>
2021-03-02 15:51:40 -0500

    add env and gitignore

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,11 +3,12 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 type Config struct {
 	serverAddr       string
-	serverPort       int
+	serverPort       string
 	MongoURL         string
 	Database         string
 	LessonCollection string
@@ -16,14 +17,15 @@ type Config struct {
 
 func Get() *Config {
 	conf := &Config{}
+	uri := fmt.Sprintf("mongodb+srv://%s:%s@%s", os.Getenv("MONGODB_USERNAME"), os.Getenv("MONGODB_PASSWORD"), os.Getenv("MONGODB_URI"))
 
-	flag.StringVar(&conf.serverAddr, "serverAddr", "localhost", "server host")
-	flag.IntVar(&conf.serverPort, "serverPort", 8080, "server port")
+	flag.StringVar(&conf.serverAddr, "serverAddr", os.Getenv("SERVER_HOST"), "server host")
+	flag.StringVar(&conf.serverPort, "serverPort", os.Getenv("SERVER_PORT"), "server port")
 	// flag.StringVar(&conf.MongoURL, "mongoURL", "mongodb://127.0.0.1:27017", "mongodb url")
-	flag.StringVar(&conf.MongoURL, "mongoURL", "mongodb+srv://chao:vedUDwrvzqcz4BjD@dev-pl-0.mqwfb.mongodb.net/test", "mongodb url")
+	flag.StringVar(&conf.MongoURL, "mongoURL", uri, "mongodb url")
 	// flag.StringVar(&conf.Database, "database", "Lessons", "datbase")
-	flag.StringVar(&conf.Database, "database", "test", "datbase")
-	flag.StringVar(&conf.LessonCollection, "lessonCollection", "lesson", "database collecion of lesson")
+	flag.StringVar(&conf.Database, "database", os.Getenv("DB_NAME"), "datbase")
+	flag.StringVar(&conf.LessonCollection, "lessonCollection", os.Getenv("DB_COLLECTION"), "database collecion of lesson")
 	flag.StringVar(&conf.ModelCollection, "modelCollection", "model", "database collecion of model")
 
 	flag.Parse()
